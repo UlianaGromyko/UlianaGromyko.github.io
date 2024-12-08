e872fec8eb8cc8428eb7ac9effe8bff80dae8cba
UlianaGromyko <ulya.gromyko@gmail.com>
2024-12-08 13:31:03 +0100

    f

--- 28d/25/sketch.js
+++ 28d/25/sketch.js
@@ -1,13 +1,13 @@
 let a = [];
 let s = [];
-let l = 100;
+let l = 20;
 let t = 0;
 
 function setup() {
   createCanvas(1790 , 960);
   for (let i = 0; i < l; i++) {
     a[i] = createVector(0, 0);
-    s[i] = map(sin(i/l*PI/2), 0, 1, 100, width*2);
+    s[i] = 200+i*50;
   }
   noStroke();
   noCursor();
@@ -21,7 +21,8 @@ function draw() {
 
   for (let i = a.length - 1; i > 0; i--) {
     a[i] = a[i - 1];
-    fill(map(s[i], 100, width*2, 255, 0));
+    fill(map(s[i], s[0], s[s.length - 1], 255, 0));
+
     circle(a[i].x, a[i].y, s[i]);
   }
   a[0] = createVector(mouseX, mouseY);
